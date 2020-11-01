d2bda4e114ba5aab47a285792ab496983b84c115
sandeepsangole <sandeepsangole@gmail.com>
2020-11-01 09:27:29 -0800

    adds print for testing

--- Shalabh/Project.py
+++ Shalabh/Project.py
@@ -9,6 +9,7 @@ import numpy as np
 # create object
 app = Flask(__name__)
 
+print("Test")
 
 # create end point to  train your model and save training data in pickle file
 @app.route('/train_model')
